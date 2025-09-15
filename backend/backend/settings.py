48d3a0e14cf2db7b74c4b4ecd3291b2629bc7201
Hammad Bin Sajjad <hammadbinsajjad2002@gmail.com>
2025-09-15 18:36:11 +0500

    Update jwt lifetimes

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -146,6 +147,11 @@ REST_FRAMEWORK = {
 
 DJOSER = {
     "TOKEN_MODEL": None,
+}
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=20),
 }
 
 
